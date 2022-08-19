e920cdee491f6a94df5a382e02f7d8af103b0ee1
thenesterov <thenesterov@proton.me>
2022-08-19 18:29:47 +0500

    Add models for book

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 2. `python3 -m venv venv`
 3. `. venv/bin/python3` (linux and macos) or `venv\Scripts\activate.bat` (windows)
 4. `pip3 install -r requirements.txt`
-5. `python3 manage.py migrate`
-6. `python3 manage.py runserver`
+5. `python3 manage.py makemigrations`
+6. `python3 manage.py migrate`
+7. `python3 manage.py runserver`
---
+++ hushbooks/book/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    short_description = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000)
+    publisher_id = models.ForeignKey(Publisher, on_delete=models.PROTECT)
+    year = models.IntegerField
+    part = models.IntegerField
+    page_count = models.IntegerField
+    table_of_content = models.JSONField
+
+    def __str__(self):
+        return self.name
+
+
+class Cover(models.Model):
+    class Size(models.TextChoices):
+        SMALL = 'sm', _('Small')
+        MEDIUM = 'md', _('Medium')
+        LARGE = 'lg', _('Large')
+        EXTRALARGE = 'xl', _('ExtraLarge')
+
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    size = models.CharField(max_length=2, choices=Size.choices, default=Size.EXTRALARGE)
+    link = models.CharField(max_length=255)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book)
