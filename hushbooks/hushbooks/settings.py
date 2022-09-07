ba06284bab3071f4e06017f6ae50e05f0b5370f9
yevkal <kaliuzhnyiyevhenii@gmail.com>
2022-09-07 16:00:52 +0300

    Add environment variables to .env

--- hushbooks/hushbooks/settings.py
+++ hushbooks/hushbooks/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,10 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!$!gyaeh7)@-v2h5qmardn^ir07rnz=ji_jzlbnl$tjjua7_dp'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast = bool)
 
 ALLOWED_HOSTS = []
 
